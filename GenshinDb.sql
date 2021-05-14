fb38ce448b40408faa5f48ecdace889c14766adb
Mattea Swain <mmswain@tql.com>
2021-05-14 19:24:37 -0400

    Added ranking row,default region,default ranking, added characters

--- GenshinDb.sql
+++ GenshinDb.sql
@@ -1,22 +1,30 @@
 -- Create Database for new project
-/*
+
+
+use master;
+
+drop database GenshinImpactDb;
+
 create database GenshinImpactDb;
-*/
+
+use GenshinImpactDb;
+
 
 -- Create table for Characters
-/*
+
 use GenshinImpactDb
 	create table Characters
 		(Id int primary key identity(1,1),
 			Name varchar(30) not null,
-				Region varchar(50), 
+				Region varchar(50) not null default 'Mondstadt',
+					Ranking int not null default 4
 					
 									
 		);
-*/
+
 
 -- Create table for Elements
-/*
+
 create table Elements
 	(Id int primary key identity(1,1),
 		CharacterId int not null foreign key references Characters(Id),
@@ -24,26 +32,36 @@ create table Elements
 
 	);
 
-*/
+
 
 -- Create weapons table
-/*
+
 create table Weapons
 	(Id int primary key identity(1,1),
 		CharacterId int not null foreign key references Characters(Id),
 			WeaponType varchar(20) not null
 	);
-*/
-/*
+
+
 use GenshinImpactDb
 
-insert into Characters
-	(Name, Region)
-	values ('Diluc', 'Mondstadt');
-*/
-/*
-use GenshinImpactDb;
+insert into Characters(Name,Ranking)
+	values ('Albedo',5);
 
-insert into Characters(Name, Region)
-	values ('Ganyu','Liyue');
-*/
+insert into Characters(Name)
+	values ('Amber');
+
+insert into Characters(Name)
+	values ('Barbara');
+
+insert into Characters
+	(Name, Ranking)
+	values ('Diluc', 5);
+
+insert into Characters(Name, Region,Ranking)
+	values ('Ganyu','Liyue',5);
+
+
+
+
+
