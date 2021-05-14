0f310eee77dbe01d91b3e96008ee9c57c81047cd
Mattea Swain <mmswain@tql.com>
2021-05-13 20:55:11 -0400

    Mattea's first version

---
+++ GenshinDb.sql
@@ -0,0 +1,36 @@
+-- Create Database for new project
+/*
+create database GenshinImpactDb;
+*/
+
+-- Create table for Characters
+/*
+use GenshinImpactDb
+	create table Characters
+		(Id int primary key identity(1,1),
+			Name varchar(30) not null,
+				Region varchar(50), 
+					
+									
+		);
+*/
+
+-- Create table for Elements
+/*
+create table Elements
+	(Id int primary key identity(1,1),
+		CharacterId int not null foreign key references Characters(Id),
+			Element varchar(20) not null, 
+
+	);
+
+*/
+
+-- Create weapons table
+/*
+create table Weapons
+	(Id int primary key identity(1,1),
+		CharacterId int not null foreign key references Characters(Id),
+			WeaponType varchar(20) not null
+	);
+/*
---
+++ join-view-Genshin-tables.sql
@@ -0,0 +1,10 @@
+--Join Genshin Tables
+
+use GenshinImpactDb;
+
+select c.Name, c.Region, e.Element,w.WeaponType 
+from Characters c
+join Elements e
+on c.Id = e.CharacterId
+join Weapons w
+on c.Id = w.CharacterId;
