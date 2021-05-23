e597baf23bf24dedf629f4836f4588fab7493a84
Charlie Traylor <ctraylor@tql.com>
2021-05-23 19:21:51 -0400

    genshintablescomplete

--- GenshinDb.sql
+++ GenshinDb.sql
@@ -1,6 +1,6 @@
 -- Create Database for new project
 
-/*
+
 use master;
 
 drop database GenshinImpactDb;
@@ -8,21 +8,6 @@ drop database GenshinImpactDb;
 create database GenshinImpactDb;
 
 use GenshinImpactDb;
-
-*/
--- Create table for Characters
-
-use GenshinImpactDb
-	create table Characters
-		(Id int primary key identity(1,1),
-			Name varchar(30) not null,
-				Region varchar(50) null default 'Mondstadt',
-					Location varchar(80) null,
-						Ranking int not null default 4
-							
-									
-		);
-
 
 -- Create table for Elements
 
@@ -32,61 +17,28 @@ create table Elements
 
 	);
 
-
-
 -- Create weapons table
 
 create table Weapons
 	(Id int primary key identity(1,1),
-		CharacterId int not null foreign key references Characters(Id),
-			WeaponType varchar(20) not null
+		WeaponType varchar(20)
+     			
 	);
 
+-- Create table for Characters
 
-
-
-alter table Characters
-	add ElementsId int null foreign key references Elements(Id);
-
-
-
-
-Begin Transaction;
-
-insert into Characters(Name,Location, Ranking)
-	values ('Albedo','Wyrmrest Valley',5);
-
-insert into Characters(Name, Location)
-	values ('Amber','Mondstadt');
-
-insert into Characters(Name, Location)
-	values ('Barbara','Mondstadt');
-
-insert into Characters(Name,Region,Location)
-	values ('Beidou', 'Inazuma','Guyun Stone Forest');
-insert into Characters(Name, Location)
-	values ('Bennett','Mondstadt');
-
-insert into Characters(Name, Location, Ranking)
-	values ('Diluc', 'Dawn Winery', 5);
-
-insert into Characters(Name,Region,Location,Ranking)
-	values ('Ganyu','Liyue','Liyue Harbor',5);
-
-commit transaction;
-
-select * from Elements
-select * from Characters
-
-
-
-insert into Characters(Name,Location,Ranking)
-	values ('Jean','Mondstadt',5);
-
-
-
-
-
+use GenshinImpactDb
+	create table Characters
+		(Id int primary key identity(1,1),
+			Name varchar(30) not null,
+				Region varchar(50) null default 'Mondstadt',
+					Location varchar(80) null,
+						Ranking int not null default 4,
+							WeaponId int null foreign key references Weapons(Id),
+								ElementsId int null foreign key references Elements(Id)
+							
+									
+		);
 
 insert into  Elements
 	(Element)
@@ -117,6 +69,54 @@ insert into  Elements
 	(Element)
 		values
 	('Hydro');
+
+	insert into Weapons (WeaponType)
+		values ('Longsword');
+	insert into Weapons (WeaponType)
+		values ('Claymore');
+	insert into Weapons (WeaponType)
+		values ('Polearm');
+	insert into Weapons (WeaponType)
+		values ('Bow');
+	insert into Weapons (WeaponType)
+		values ('Catalyst');
+
+
+
+Begin Transaction;
+
+insert into Characters(Name,Location, Ranking, WeaponId, ElementsId)
+	values ('Albedo','Wyrmrest Valley',5, 1, 2);
+
+insert into Characters(Name, Location, WeaponId, ElementsId)
+	values ('Amber','Mondstadt', 4, 4);
+
+insert into Characters(Name, Location, WeaponId, ElementsId)
+	values ('Barbara','Mondstadt', 5, 6);
+
+insert into Characters(Name,Region,Location, WeaponId, ElementsId)
+	values ('Beidou', 'Inazuma','Guyun Stone Forest', 2, 5);
+
+insert into Characters(Name, Location, WeaponId, ElementsId)
+	values ('Bennett','Mondstadt', 1, 4);
+
+insert into Characters(Name, Region, Location, Ranking, WeaponId, ElementsId)
+	values ('Xiao', 'Liyue', 'Wangshu Inn', 5, 3, 1);
+
+insert into Characters(Name,Region,Location,Ranking, WeaponId, ElementsId)
+	values ('Ganyu','Liyue','Liyue Harbor',5, 4, 3);
+
+commit transaction;
+
+
+
+insert into Characters(Name,Location,Ranking, WeaponId, ElementsId)
+	values ('Jean','Mondstadt',5, 1, 1);
+
+
+
+
+
 
 
 
